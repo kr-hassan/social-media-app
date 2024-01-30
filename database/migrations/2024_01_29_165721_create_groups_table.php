0956df78c70b95ef21d7d2f6afc59e439f346fa1
kr-hassan <kazirabbi1000@gmail.com>
2024-01-31 01:06:36 +0600

    group and post migration added

--- database/migrations/2024_01_29_165721_create_groups_table.php
+++ database/migrations/2024_01_29_165721_create_groups_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->string('slug', 255);
+            $table->text('about')->nullable();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('deleted_by')->constrained('users');
+            $table->timestamp('deleted_at');
             $table->timestamps();
         });
     }
