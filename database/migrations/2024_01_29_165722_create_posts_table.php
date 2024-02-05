4e78eaa870a3897b921b93c60c9bb917c37991ec
kr-hassan <kazirabbi1000@gmail.com>
2024-02-05 23:44:55 +0600

    User Database modification

--- database/migrations/2024_01_29_165722_create_posts_table.php
+++ database/migrations/2024_01_29_165722_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->longText('body')->nullable();
             $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('group_id')->constrained('groups');
+            $table->foreignId('group_id')->nullable()->constrained('groups');
             $table->foreignId('deleted_by')->nullable()->constrained('users');
             $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
